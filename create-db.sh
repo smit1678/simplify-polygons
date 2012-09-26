aab9e72d805bbdc97c4e2bfe9d0ac0f6c53848f8
Nate Smith <nate@developmentseed.org>
2012-09-26 09:47:49 -0400

    removed absolute paths

--- create-db.sh
+++ create-db.sh
@@ -2,13 +2,13 @@
 
 createdb $1
 createlang plpgsql $1
-psql -d $1 -f /usr/local/share/postgis/postgis.sql
-psql -d $1 -f /usr/local/share/postgis/postgis_comments.sql
-psql -d $1 -f /usr/local/share/postgis/spatial_ref_sys.sql
-psql -d $1 -f /usr/local/share/postgis/rtpostgis.sql
-psql -d $1 -f /usr/local/share/postgis/raster_comments.sql
-psql -d $1 -f /usr/local/share/postgis/topology.sql
-psql -d $1 -f /usr/local/share/postgis/topology_comments.sql
+psql -d $1 -f postgis/postgis.sql
+psql -d $1 -f postgis/postgis_comments.sql
+psql -d $1 -f postgis/spatial_ref_sys.sql
+psql -d $1 -f postgis/rtpostgis.sql
+psql -d $1 -f postgis/raster_comments.sql
+psql -d $1 -f postgis/topology.sql
+psql -d $1 -f postgis/topology_comments.sql
 
 psql -d $1 -f simplifyedge-function.sql
 
